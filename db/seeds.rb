94d019b0344a8d1bc1d64db7c441d3df1d32c047
sesaba23 <sesaba23@gmail.com>
2017-08-31 19:33:10 +0200

    Some tests seeding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,19 @@ Document.create(doc_type: "text",
                 text: "GLaDOS is a sentient computer. She promisses cake, but the cake is a lie.")
 
 # # Create fake user for development porposes
-# User.create!(name:  "sesaba23",
-#             email: "sesaba23@gmail.com",
-#             password:              "foobar",
-#             password_confirmation: "foobar",
-#             admin: true)
+ User.create!(name:  "sesaba23",
+             email: "sesaba23@gmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
    
-# 99.times do |n|
-#      name  = Faker::Name.name
-#      email = "example-#{n+1}@railstutorial.org"
-#      password = "password"
-#      User.create!(name:  name,
-#                   email: email,
-#                   password:              password,
-#                   password_confirmation: password,
-#                   admin: false)
-# end
+ 3.times do |n|
+      name  = Faker::Name.name
+      email = "user-#{n+1}@team-red.org"
+      password = "password"
+      User.create!(name:  name,
+                   email: email,
+                   password:              foobar,
+                   password_confirmation: foobar,
+                   admin: false)
+ end
