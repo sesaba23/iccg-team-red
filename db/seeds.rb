ecd6081b51fb6122cf8d91a7220ec9344def0efe
kirmesh <kirmesh@gmail.com>
2017-09-07 17:22:10 +0200

    added a fable to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,25 @@
 
 Document.create(doc_type: "text",
                 text: "Muad'Dib is a fictional species of desert mouse. The people of the desert respect Muad'Dib, because Muad'Dib is wise in the ways of the desert: Muad'Dib creates his own water. Muad'Dib hides from the sun and travels in the cool night. Muad'Dib is fruitful and multiplies over the land.")
-Document.create(doc_type: "text",
-                text: "GLaDOS is a sentient computer. She promisses cake, but the cake is a lie.")
+
+text = "The Ant and the Dove 
+
+
+  AN ANT went to the bank of a river to quench its thirst, and
+being carried away by the rush of the stream, was on the point of
+drowning.  A Dove sitting on a tree overhanging the water plucked
+a leaf and let it fall into the stream close to her.  The Ant
+climbed onto it and floated in safety to the bank.  Shortly
+afterwards a birdcatcher came and stood under the tree, and laid
+his lime-twigs for the Dove, which sat in the branches.  The Ant,
+perceiving his design, stung him in the foot.  In pain the
+birdcatcher threw down the twigs, and the noise made the Dove
+take wing.
+
+
+	One good turn deserves another"
+
+Document.create(doc_type: "text", text: text)
 
 User.create!(name:  "Kirill",
             email: "kirmesh@gmail.com",
