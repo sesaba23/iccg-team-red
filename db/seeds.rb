999ba07b7436bf87c98dce1c7c91f457187f238c
David O' Rojo <david.orojo@gmail.com>
2017-09-28 01:19:04 -0500

    Seed fables information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# The data can then be loaded with the rails db:seed command (or created
+# alongside the database with db:setup).
 
-Document.create(doc_type: "text",
-                text: "Muad'Dib is a fictional species of desert mouse. The people of the desert respect Muad'Dib, because Muad'Dib is wise in the ways of the desert: Muad'Dib creates his own water. Muad'Dib hides from the sun and travels in the cool night. Muad'Dib is fruitful and multiplies over the land.")
+# Load the Aesop's fables from /db/documents
+Dir.glob('db/documents/*.txt').each do |file_name|
+  lines = File.open(file_name, 'r').readlines
+  title = lines[0].chomp
+  content = lines.slice(2, lines.length - 2).join.chomp
 
-text = "The Ant and the Dove 
+  Document.create title: title, content: content, kind: 'plain text'
+end
 
-
-  AN ANT went to the bank of a river to quench its thirst, and
-being carried away by the rush of the stream, was on the point of
-drowning.  A Dove sitting on a tree overhanging the water plucked
-a leaf and let it fall into the stream close to her.  The Ant
-climbed onto it and floated in safety to the bank.  Shortly
-afterwards a birdcatcher came and stood under the tree, and laid
-his lime-twigs for the Dove, which sat in the branches.  The Ant,
-perceiving his design, stung him in the foot.  In pain the
-birdcatcher threw down the twigs, and the noise made the Dove
-take wing.
-
-
-	One good turn deserves another"
-
-Document.create(doc_type: "text", text: text)
 
 User.create!(name:  "Kirill",
             email: "kirmesh@gmail.com",
@@ -53,7 +39,7 @@ User.create!(name:  "Sam",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
-   
+
  3.times do |n|
       #name  = Faker::Name.name
       name = "user-#{n+1}"
